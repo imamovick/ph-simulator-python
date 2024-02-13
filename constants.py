ba66c477e0f9809f07db07bd3417a404389397c2
carlos-garciamoran <46545682+carlos-garciamoran@users.noreply.github.com>
2024-02-13 14:06:01 -0800

    constants drop

--- constants.py
+++ constants.py
@@ -1,2 +1,15 @@
-# Define constants here
-x = 3.14
+constants = {
+    "Kw": 1e-14,
+    "Ka_acetic_acid": 1.8e-5,
+    "Ka_carbonic_acid": 4.3e-7,
+    "Ka_bicarbonate": 5.6e-11,  # Used for NaHCO3
+    "Ka_dihydrogen_phosphate": 6.2e-8,  # Used for NaH2PO4
+    "Ka_hydrogen_sulfate": 1.2e-2,  # Used for NaHSO4
+    "Kb_ammonia": 1.8e-5,
+
+    "pKa_acetic_acid": 4.745,
+    "pKa_ammonium_chloride": 9.255,
+    "pKa_monohydrogen_phosphate": 7.208,
+    "pKa_bicarbonate": 10.252,
+    "pKa_carbonic_acid": 6.367
+}
