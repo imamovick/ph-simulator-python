456fbd2e0ab0b62130606789b18c40c0567dcdba
lc20-0103 <lc20-0103@lclark.edu>
2024-02-29 14:14:14 -0800

    # final iteration of the .py's with concentration and method name refinements.

--- calculations.py
+++ calculations.py
@@ -48,59 +48,59 @@ def get_pKb(Kb):
     return -math.log10(Kb)
 
 # Barium Hydroxide, strong base
-def get_Ba_OH_2(Ba_OH_2_conc):
+def get_Ba_OH_2_Hplus(Ba_OH_2_conc):
     return Kw / (2*Ba_OH_2_conc)
 
 # Calcium Hydroxide, strong base
-def get_CA_OH_2(CA_OH_2_conc):
+def get_CA_OH_2_Hplus(CA_OH_2_conc):
     return Kw / (2*CA_OH_2_conc)
 
 # Sodium Hydroxide, strong base
-def get_NaOH(NaOH_conc):
+def get_NaOH_Hplus(NaOH_conc):
     return Kw / NaOH_conc
 
 # Ammonium Hydroxide, weak base
-def get_NH4OH(NH4OH_conc):
+def get_NH4OH_Hplus(NH4OH_conc):
     return Kw/math.sqrt(Kb_ammonia*NH4OH_conc)
 
 # Hydrochloric Acid, strong acid
-def get_HCl(HCl_conc):
+def get_HCl_Hplus(HCl_conc):
     return HCl_conc
 
 # Nitric Acid, strong acid
-def get_HNO3(HNO3_conc):
+def get_HNO3_Hplus(HNO3_conc):
     return HNO3_conc
 
 # Acetic Acid, weak acid
-def get_HC2H3O2(HC2H3O2_conc):
+def get_HC2H3O2_Hplus(HC2H3O2_conc):
     return math.sqrt(Ka_acetic_acid*HC2H3O2_conc)
 
 # Carbonic Acid, weak acid
-def get_H2CO3(H2CO3_conc):
+def get_H2CO3_Hplus(H2CO3_conc):
     return math.sqrt(Ka_carbonic_acid*H2CO3_conc)
 
 # Sodium Chloride, neutral salt
-def get_NaCl():
+def get_NaCl_Hplus():
     return 7
 
 # Ammonium Chloride, acidic salt of NH3
-def get_NH4Cl(NH4Cl_conc):
+def get_NH4Cl_Hplus(NH4Cl_conc):
     return math.sqrt((Kw / Kb_ammonia) * NH4Cl_conc)
 
 # Sodium Acetate, basic salt of HC2H3O2
-def get_NaC2H3O2(NaC2H3O2_conc):
+def get_NaC2H3O2_Hplus(NaC2H3O2_conc):
     return Kw / math.sqrt((Kw / Ka_acetic_acid) * NaC2H3O2_conc)
 
 # Sodium Bicarbonate, basic salt H2CO3
-def get_NaHCO3(NaHCO3_conc):
+def get_NaHCO3_Hplus(NaHCO3_conc):
     return Kw / math.sqrt(Ka_carbonic_acid * NaHCO3_conc)
 
 # Sodium Carbonate, basic salt of HCO3
-def get_Na2CO3(Na2CO3_conc):
+def get_Na2CO3_Hplus(Na2CO3_conc):
     return Kw / math.sqrt((Kw / Ka_bicarbonate) * Na2CO3_conc)
 
 # Sodium Bisulfate, acidic salt of H2SO4
-def get_NaHSO4(NaHSO4_conc):
+def get_NaHSO4_Hplus(NaHSO4_conc):
     return math.sqrt(Ka_hydrogen_sulfate * NaHSO4_conc)
 
 def get_volume_added(drops):
@@ -109,6 +109,7 @@ def get_volume_added(drops):
 def get_total_volume(drops):
     return 10 + get_volume_added(drops)
 
+# 0.1 or 0.01 M
 def get_HCl_H(M_HCl, drops):
     return ((M_HCl * get_volume_added(drops)) / get_total_volume(drops)) + 1e-7
 def get_NaOH_H(M_NaOH, drops):
@@ -117,10 +118,8 @@ def get_NaOH_H(M_NaOH, drops):
 '''
 For Hc2H3O2 (Acetic Acid) / NaC2H3O2 (Sodium Acetate) Buffer System
 '''
-# REWORK!!
-# seeing as though it requires acetic acid and sodium acetate which can be calculated in two diff ways
 def get_ace_buffer_system(NaC2H3O2_conc, HC2H3O2_conc):
-    return pKa_acetic_acid + math.log10(get_NaC2H3O2(NaC2H3O2_conc)/get_HC2H3O2(HC2H3O2_conc))
+    return pKa_acetic_acid + math.log10(get_NaC2H3O2_Hplus(NaC2H3O2_conc) / get_HC2H3O2_Hplus(HC2H3O2_conc))
 
 # Adding drops of .1 or .01 M HCl
 def get_HCl_HC2H3O2(HC2H3O2_conc, M_HCl, drops):
@@ -141,14 +140,13 @@ def get_NaC2H3O2_init_M(NaC2H3O2_conc):
 def get_M_HCl(drops, M_HCl):
     return 0.36 * drops * M_HCl
 
-def get_excess_H(drops, M_HCl, NaC2H3O2_conc):
+def get_excess_Hplus(drops, M_HCl, NaC2H3O2_conc):
     return (get_M_HCl(drops, M_HCl)) - (get_NaC2H3O2_init_M(NaC2H3O2_conc))
 
 # H+ method defined in the NaC2H3O2 buffer capacity, using the excess H+
 def get_Hplus_fe(drops, M_HCl, NaC2H3O2_conc):
-    Hplus = (get_excess_H(drops, M_HCl, NaC2H3O2_conc)) / (get_total_volume(drops))
+    Hplus = (get_excess_Hplus(drops, M_HCl, NaC2H3O2_conc)) / (get_total_volume(drops))
     return Hplus
-
 # HC2H3O2 buffer capacity calculations
 # Adding drops of .1 or .01 NaOH
 
@@ -228,5 +226,3 @@ def get_acid_init_M(acid_conc):
 def get_excess_OH(drops, acid_conc):
     return get_M_NaOH(drops, M_NaOH) - get_acid_init_M(acid_conc)
 
-# REWORK BUFFER SYSTEMS TO ALL USE Hplus, excessHplus, OH, AND excessOH
-# AS IT STANDS THEY ARE INCONSISTENT
